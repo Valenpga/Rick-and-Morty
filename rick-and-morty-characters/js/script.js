59e6dc000aa9d9f50cc7654740a82b9a1c79a2ea
Valenpga <131762425+Valenpga@users.noreply.github.com>
2023-11-17 19:07:13 +0100

    Codigo cambiado script.js

--- rick-and-morty-characters/js/script.js
+++ rick-and-morty-characters/js/script.js
@@ -17,6 +17,52 @@ let pagina = 1;
 ButtonPrev.addEventListener('click', function() { pagina = pagina + 1; fetchPersonajes(pagina); });
 
 ButtonNex .addEventListener('click', function() { pagina = pagina - 1; fetchPersonaje(pagina); });
+const characterList = document.getElementById('characterList')
+const prevPageButton = document.getElementById('prev-page');
+const nexPageButton = document.getElementById('next-page');
+
+let currentPage = 1
+
+const getCharactersRickAndMorty = (page) => {
+    fetch('https://rickandmortyapi.com/api/character/?page=${page}')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('La solicitud no se puede procesar')
+        }    return response.json()
+        
+    })
+    .then(data =>{
+        const characters = data.results
+        characterList.innerHTML = ''
+        characters.forEach(element => {
+            characterList.innerHTML += `
+                <li>
+                    <figure>
+                        <img src="${element.image}" alt="${element.name}"/>
+                        <figcaption>
+                            <p><span>Name:</span>${element.name}</p>
+                            <p><span>Species:</span>${element.species}</p>
+                        </figcaption>
+                    </figure>
+               </li>`
+
+        })
+    })
+    
+}
+
+nextPageButton.addEventListener('click',function(){
+    if(currentPage < 42)
+        getCharactersRickAndMorty(currentPage ++);
+})
+prevPageButton.addEventListener('click', function(){
+    if(currentPage > 1)
+        getCharactersRickAndMorty(currentPage --);
+})
+
+getCharactersRickAndMorty(currentPage)
+
+
     
    
 
